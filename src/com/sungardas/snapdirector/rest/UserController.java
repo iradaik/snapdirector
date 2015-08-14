d011cad65668ac6062a50ed7efeb07d68d443a4e
Kostiantyn Glukhenko <glukhenko.kostiantyn@gmail.com>
2015-08-14 14:45:56 +0300

    SNAP-69 and SNAP-70 As User I can see list of the user accounts

--- src/com/sungardas/snapdirector/rest/UserController.java
+++ src/com/sungardas/snapdirector/rest/UserController.java
@@ -1,7 +1,12 @@
 package com.sungardas.snapdirector.rest;
 
 import com.sungardas.snapdirector.aws.dynamodb.model.User;
+import com.sungardas.snapdirector.dto.UserDto;
 import com.sungardas.snapdirector.aws.dynamodb.repository.UserRepository;
+import com.sungardas.snapdirector.exception.DataAccessException;
+import com.sungardas.snapdirector.service.UserService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 
 @RestController
@@ -18,6 +28,9 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
 
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<String> createUser(@ModelAttribute("user") User newUser) {
@@ -37,4 +50,13 @@ public class UserController {
         return responseEntity;
     }
 
+
+    @RequestMapping(method = GET)
+    public ResponseEntity getAllUsers(){
+        try {
+            return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+        } catch (DataAccessException e){
+            return new ResponseEntity<>(Collections.emptyList(), HttpStatus.NOT_ACCEPTABLE);
+        }
+    }
 }
