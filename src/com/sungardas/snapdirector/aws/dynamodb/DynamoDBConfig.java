9427f92a5b27ed481ea6fae806eaeda4cbac27b0
Iryna Radaikina <iradaik@softserveinc.com>
2015-08-14 09:19:17 +0300

    Update AmazonDynamoDB bean creation, move region to property file

--- src/com/sungardas/snapdirector/aws/dynamodb/DynamoDBConfig.java
+++ src/com/sungardas/snapdirector/aws/dynamodb/DynamoDBConfig.java
@@ -1,12 +1,12 @@
 package com.sungardas.snapdirector.aws.dynamodb;
 
-import com.amazonaws.ClientConfiguration;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.retry.PredefinedRetryPolicies;
-import com.amazonaws.retry.RetryPolicy;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.sungardas.snapdirector.aws.EnvironmentBasedCredentialsProvider;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -22,10 +22,14 @@ public class DynamoDBConfig {
     @Value("${amazon.aws.secretkey}")
     private String amazonAWSSecretKey;
 
+    @Value("${region}")
+    private String region;
+
+
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
-            AmazonDynamoDB amazonDynamoDB = new AmazonDynamoDBClient(
-                    amazonAWSCredentials(), new ClientConfiguration().withRetryPolicy(new RetryPolicy(null, null, 3, true)));
+        AmazonDynamoDB amazonDynamoDB = new AmazonDynamoDBClient(new EnvironmentBasedCredentialsProvider());
+        amazonDynamoDB.setRegion(Region.getRegion(Regions.fromName(region)));
         return amazonDynamoDB;
     }
 
